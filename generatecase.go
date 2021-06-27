b3ce8c0de3203907d090d9ab0433ac03fca7bba8
fishingfly <zhouyuzf@163.com>
2021-06-27 08:08:28 +0800

    解决map拷贝问题

--- generatecase.go
+++ generatecase.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"math"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
 // node1, node3
 // node2
 func GetCase1() CaseDemo {
@@ -83,9 +90,12 @@ func GetCase2() CaseDemo {
 	}
 }
 
-// node1, node4
-// node2,node5
-// node3
+/*
+group #:[ node2,node5,]
+group #:[ node1,]
+group #:[ node3,]
+group #:[ node4,]
+*/
 func GetCase3() CaseDemo {
 	apps := []Application{
 		{NodeName: "node1", AppName: "app1"},
@@ -100,9 +110,9 @@ func GetCase3() CaseDemo {
 		{NodeName: "node2", AppName: "app4"},
 		{NodeName: "node3", AppName: "app4"},
 		{NodeName: "node4", AppName: "app4"},
-		{NodeName: "node3", AppName: "app4"},
-		{NodeName: "node4", AppName: "app4"},
-		{NodeName: "node5", AppName: "app4"},
+		{NodeName: "node3", AppName: "app5"},
+		{NodeName: "node4", AppName: "app5"},
+		{NodeName: "node5", AppName: "app5"},
 	}
 	nodes := []Node{
 		{"node1"},
@@ -132,6 +142,355 @@ func GetCase3() CaseDemo {
 			DisruptionAllowed: 1,
 			AppName: "app5",
 		},
+	}
+	return CaseDemo{
+		Nodes: nodes,
+		Apps: apps,
+		DisruptionBudgets: budgets,
+	}
+}
+
+/*
+group #:[ node2,node5,]
+group #:[ node1,]
+group #:[ node3,]
+group #:[ node4,]
+*/
+func GetCase4() CaseDemo {
+	apps := []Application{
+		{NodeName: "node1", AppName: "app1"},
+		{NodeName: "node2", AppName: "app1"},
+		{NodeName: "node3", AppName: "app1"},
+		{NodeName: "node2", AppName: "app2"},
+		{NodeName: "node3", AppName: "app2"},
+		{NodeName: "node4", AppName: "app3"},
+		{NodeName: "node5", AppName: "app3"},
+		{NodeName: "node1", AppName: "app3"},
+		{NodeName: "node2", AppName: "app4"},
+		{NodeName: "node3", AppName: "app4"},
+		{NodeName: "node4", AppName: "app4"},
+		{NodeName: "node3", AppName: "app5"},
+		{NodeName: "node5", AppName: "app5"},
+	}
+	nodes := []Node{
+		{"node1"},
+		{"node2"},
+		{"node3"},
+		{"node4"},
+		{"node5"},
+	}
+	budgets := []DisruptionBudget{
+		{
+			DisruptionAllowed: 1,
+			AppName: "app1",
+		},
+		{
+			DisruptionAllowed: 1,
+			AppName: "app2",
+		},
+		{
+			DisruptionAllowed: 1,
+			AppName: "app3",
+		},
+		{
+			DisruptionAllowed: 1,
+			AppName: "app4",
+		},
+		{
+			DisruptionAllowed: 1,
+			AppName: "app5",
+		},
+	}
+	return CaseDemo{
+		Nodes: nodes,
+		Apps: apps,
+		DisruptionBudgets: budgets,
+	}
+}
+
+
+/*
+group #:[ node1,node4,]
+group #:[ node2,node5,]
+group #:[ node3,]
+*/
+func GetCase5() CaseDemo {
+	apps := []Application{
+		{NodeName: "node1", AppName: "app1"},
+		{NodeName: "node2", AppName: "app1"},
+		{NodeName: "node3", AppName: "app1"},
+		{NodeName: "node2", AppName: "app2"},
+		{NodeName: "node3", AppName: "app2"},
+		{NodeName: "node4", AppName: "app3"},
+		{NodeName: "node5", AppName: "app3"},
+		{NodeName: "node2", AppName: "app4"},
+		{NodeName: "node3", AppName: "app4"},
+		{NodeName: "node4", AppName: "app4"},
+		{NodeName: "node3", AppName: "app5"},
+		{NodeName: "node5", AppName: "app5"},
+	}
+	nodes := []Node{
+		{"node1"},
+		{"node2"},
+		{"node3"},
+		{"node4"},
+		{"node5"},
+	}
+	budgets := []DisruptionBudget{
+		{
+			DisruptionAllowed: 1,
+			AppName: "app1",
+		},
+		{
+			DisruptionAllowed: 1,
+			AppName: "app2",
+		},
+		{
+			DisruptionAllowed: 1,
+			AppName: "app3",
+		},
+		{
+			DisruptionAllowed: 1,
+			AppName: "app4",
+		},
+		{
+			DisruptionAllowed: 1,
+			AppName: "app5",
+		},
+	}
+	return CaseDemo{
+		Nodes: nodes,
+		Apps: apps,
+		DisruptionBudgets: budgets,
+	}
+}
+
+/*
+group #:[ node1,node2,node3,]
+group #:[ node4,node5,node6,]
+
+*/
+func GetCase6() CaseDemo {
+	apps := []Application{
+		{NodeName: "node1", AppName: "app1"},
+		{NodeName: "node3", AppName: "app1"},
+		{NodeName: "node6", AppName: "app1"},
+		{NodeName: "node5", AppName: "app1"},
+		{NodeName: "node1", AppName: "app2"},
+		{NodeName: "node2", AppName: "app2"},
+		{NodeName: "node4", AppName: "app2"},
+		{NodeName: "node6", AppName: "app2"},
+		{NodeName: "node1", AppName: "app3"},
+		{NodeName: "node3", AppName: "app3"},
+		{NodeName: "node5", AppName: "app3"},
+		{NodeName: "node6", AppName: "app3"},
+		{NodeName: "node1", AppName: "app4"},
+		{NodeName: "node2", AppName: "app4"},
+		{NodeName: "node3", AppName: "app4"},
+		{NodeName: "node4", AppName: "app4"},
+		{NodeName: "node5", AppName: "app4"},
+	}
+	nodes := []Node{
+		{"node1"},
+		{"node2"},
+		{"node3"},
+		{"node4"},
+		{"node5"},
+		{"node6"},
+	}
+	budgets := []DisruptionBudget{
+		{
+			DisruptionAllowed: 2,
+			AppName: "app1",
+		},
+		{
+			DisruptionAllowed: 2,
+			AppName: "app2",
+		},
+		{
+			DisruptionAllowed: 2,
+			AppName: "app3",
+		},
+		{
+			DisruptionAllowed: 3,
+			AppName: "app4",
+		},
+	}
+	return CaseDemo{
+		Nodes: nodes,
+		Apps: apps,
+		DisruptionBudgets: budgets,
+	}
+}
+
+/*
+group #:[ node2,node1,]
+group #:[ node4,node5,]
+group #:[ node6,node3,]
+
+*/
+func GetCase7() CaseDemo {
+	apps := []Application{
+		{NodeName: "node1", AppName: "app1"},
+		{NodeName: "node3", AppName: "app1"},
+		{NodeName: "node4", AppName: "app1"},
+		{NodeName: "node5", AppName: "app1"},
+		{NodeName: "node2", AppName: "app2"},
+		{NodeName: "node4", AppName: "app2"},
+		{NodeName: "node6", AppName: "app2"},
+		{NodeName: "node1", AppName: "app3"},
+		{NodeName: "node3", AppName: "app3"},
+		{NodeName: "node5", AppName: "app3"},
+		{NodeName: "node2", AppName: "app4"},
+		{NodeName: "node6", AppName: "app4"},
+		{NodeName: "node3", AppName: "app5"},
+		{NodeName: "node4", AppName: "app5"},
+	}
+	nodes := []Node{
+		{"node1"},
+		{"node2"},
+		{"node3"},
+		{"node4"},
+		{"node5"},
+		{"node6"},
+	}
+	budgets := []DisruptionBudget{
+		{
+			DisruptionAllowed: 2,
+			AppName: "app1",
+		},
+		{
+			DisruptionAllowed: 1,
+			AppName: "app2",
+		},
+		{
+			DisruptionAllowed: 1,
+			AppName: "app3",
+		},
+		{
+			DisruptionAllowed: 1,
+			AppName: "app4",
+		},
+		{
+			DisruptionAllowed: 1,
+			AppName: "app5",
+		},
+	}
+	return CaseDemo{
+		Nodes: nodes,
+		Apps: apps,
+		DisruptionBudgets: budgets,
+	}
+}
+
+/*
+node7上无app
+
+group #:[ node1,node2,node3,node7,]
+group #:[ node6,node4,node5,]
+
+*/
+func GetCase8() CaseDemo {
+	apps := []Application{
+		{NodeName: "node1", AppName: "app1"},
+		{NodeName: "node3", AppName: "app1"},
+		{NodeName: "node6", AppName: "app1"},
+		{NodeName: "node5", AppName: "app1"},
+		{NodeName: "node1", AppName: "app2"},
+		{NodeName: "node2", AppName: "app2"},
+		{NodeName: "node4", AppName: "app2"},
+		{NodeName: "node6", AppName: "app2"},
+		{NodeName: "node1", AppName: "app3"},
+		{NodeName: "node3", AppName: "app3"},
+		{NodeName: "node5", AppName: "app3"},
+		{NodeName: "node6", AppName: "app3"},
+		{NodeName: "node1", AppName: "app4"},
+		{NodeName: "node2", AppName: "app4"},
+		{NodeName: "node3", AppName: "app4"},
+		{NodeName: "node4", AppName: "app4"},
+		{NodeName: "node5", AppName: "app4"},
+	}
+	nodes := []Node{
+		{"node1"},
+		{"node2"},
+		{"node3"},
+		{"node4"},
+		{"node5"},
+		{"node6"},
+		{"node7"},
+	}
+	budgets := []DisruptionBudget{
+		{
+			DisruptionAllowed: 2,
+			AppName: "app1",
+		},
+		{
+			DisruptionAllowed: 2,
+			AppName: "app2",
+		},
+		{
+			DisruptionAllowed: 2,
+			AppName: "app3",
+		},
+		{
+			DisruptionAllowed: 3,
+			AppName: "app4",
+		},
+	}
+	return CaseDemo{
+		Nodes: nodes,
+		Apps: apps,
+		DisruptionBudgets: budgets,
+	}
+}
+
+func generateCase(nodeNum, appKinds int) CaseDemo {
+	nodes := make([]Node, 0)
+	for i := 0; i < nodeNum; i++ {
+		nodes = append(nodes,Node{
+			NodeName: "node" + strconv.Itoa(i),
+		})
+	}
+	apps := make([]Application, 0)
+	app2Count := make(map[int]int)
+	for i := 0; i < appKinds; i++ {
+		rand.Seed(time.Now().Unix())
+		appNums := rand.Intn(appKinds - 1)
+		app2Count[i] = appNums
+		if appNums <= 0 { // 最少10个
+			appNums = 1
+		}
+		record := make(map[int]int)
+		for j := 0; j < appNums; j++ {
+			nodeNo := 1
+			for {
+				rand.Seed(time.Now().Unix())
+				nodeNo = rand.Intn(nodeNum)
+				if nodeNo == 0 {
+					nodeNo = 1
+				}
+				// 避免重复
+				if _, ok := record[nodeNo]; ok {
+					continue
+				} else {
+					break
+				}
+			}
+			record[nodeNo] = 1
+			apps = append(apps, Application{
+				NodeName: "node" + strconv.Itoa(nodeNo),
+				AppName: "app" + strconv.Itoa(j),
+			})
+		}
+	}
+
+	budgets := make([]DisruptionBudget, 0)
+
+	for i := 0; i < appKinds; i++ {
+		budgets = append(budgets, DisruptionBudget{
+			DisruptionAllowed: int(math.Ceil(float64(app2Count[i]) * 0.2)),
+			AppName:  "app" + strconv.Itoa(i),
+		})
 	}
 	return CaseDemo{
 		Nodes: nodes,
--- main.go
+++ main.go
@@ -7,18 +7,21 @@ import (
 )
 
 func main() {
-	GetSolution(GetCase3())
+	//GetOptimalUpgradePlans(GetCase8())
+	GetOptimalUpgradePlans(generateCase(100, 40))
 }
 
-func GetSolution(caseDemo CaseDemo){
+func GetOptimalUpgradePlans(caseDemo CaseDemo){
 	apps := caseDemo.Apps
 	nodes := caseDemo.Nodes
 	budgets := caseDemo.DisruptionBudgets
-	timer :=time.NewTicker(time.Minute * 2)
+	startTime := time.Now()
+	timer :=time.NewTicker(time.Minute * 10)
 	for {
 		select {
 		case <-timer.C:
-			fmt.Println("time out")
+			// 超时退出
+			fmt.Println("time out, failed to GetOptimalUpgradePlans ")
 			break
 		default:
 
@@ -28,8 +31,15 @@ func GetSolution(caseDemo CaseDemo){
 			fmt.Println(err.Error())
 			return
 		}
-		if len(nodes) == 0 {
+		if len(nodes) == 0 { // 剪枝无节点就退出
 			break
+		}
+		if len(apps) == 0 { // 剪枝，有节点，但是无app了，那么重启方案就是所有节点
+			fmt.Print("group #:[ ")
+			for _, v := range nodes {
+				fmt.Print(v.NodeName + ",")
+			}
+			fmt.Print("]\n")
 		}
 		err, resultMap := cu.GetMaxNodesToRestart()
 		if err != nil {
@@ -37,15 +47,15 @@ func GetSolution(caseDemo CaseDemo){
 			return
 		}
 		// 打印一次结果即重启nodes
-		fmt.Print("start to restart nodes: ")
+		fmt.Print("group #:[ ")
 		for k, _ := range resultMap {
 			fmt.Print(k+",")
 		}
-		fmt.Println()
-		time.Sleep(2 * time.Second)
+		fmt.Print("]\n")
 		// 清理已重启node
 		nodes, apps = cu.CleanNodes(resultMap)
 	}
+	fmt.Println(time.Now().Sub(startTime).Microseconds())
 }
 
 type ClusterUpgrade struct {
@@ -54,9 +64,13 @@ type ClusterUpgrade struct {
 	//SelectNodeAppsCountMap map[string]int // 已选择的节点， app的数量记录
 	AppsDisruptionAllowedNum map[string]int // 每个app保证可用性下最大可是失去的应用数
 	// 缓存以i结尾的node最大可以重启的节点数
-	DynamicMap map[int]Result // 动态规划的中间量保存
+	DynamicMap map[int][]Result // 动态规划的中间量保存,<第i个节点,重启节点集>
+	NodeIResult Result // 记录从节点0到节点i之间可重启的最大节点集
 	Nodes []Node
 	Apps []Application
+	// 存放最有结果结果
+	RestartPolicyResult []string
+	MinRestartCount int
 }
 
 func NewClusterUpgrade(caseDemo CaseDemo) (error, *ClusterUpgrade) {
@@ -88,8 +102,10 @@ func NewClusterUpgrade(caseDemo CaseDemo) (error, *ClusterUpgrade) {
 		//SelectNodeAppsCountMap : make(map[string]int),
 		AppsDisruptionAllowedNum: appsAllowedCountMap,
 		Nodes: caseDemo.Nodes,
-		DynamicMap: make(map[int]Result),
+		DynamicMap: make(map[int][]Result),
 		Apps: caseDemo.Apps,
+		MinRestartCount: len(nodeMap),
+		RestartPolicyResult: make([]string, 0),
 	}
 }
 
@@ -116,6 +132,24 @@ func (cu *ClusterUpgrade)CheckNodeAvailability(nodeName string, apps []string, S
 	if len(apps) == 0 { // 说明节点上没有app，那该节点随时可重启
 		return true
 	}
+	SelectNodeAppsCountMapTemp := SelectNodeAppsCountMap
+	for i := 0; i < len(apps); i++ {
+		if _, ok := SelectNodeAppsCountMapTemp[apps[i]]; !ok {
+			SelectNodeAppsCountMapTemp[apps[i]] = 1
+			continue
+		}
+		if (SelectNodeAppsCountMapTemp[apps[i]] + 1) <= cu.AppsDisruptionAllowedNum[apps[i]] { // 未超出app预算
+			SelectNodeAppsCountMapTemp[apps[i]]++
+			continue
+		} else {// 超出预算
+			return false
+		}
+	}
+	return true
+}
+
+// 加入一个节点
+func (cu *ClusterUpgrade)addAppCount(nodeName string, apps []string, SelectNodeAppsCountMap map[string]int) {
 	for i := 0; i < len(apps); i++ {
 		if _, ok := SelectNodeAppsCountMap[apps[i]]; !ok {
 			SelectNodeAppsCountMap[apps[i]] = 1
@@ -123,15 +157,16 @@ func (cu *ClusterUpgrade)CheckNodeAvailability(nodeName string, apps []string, S
 		}
 		if (SelectNodeAppsCountMap[apps[i]] + 1) <= cu.AppsDisruptionAllowedNum[apps[i]] { // 未超出app预算
 			SelectNodeAppsCountMap[apps[i]]++
-		} else {// 超出预算
-			//先回退再返回
-			for j := 0; j < i; j++ {
-				SelectNodeAppsCountMap[apps[j]]--
-			}
-			return false
+			continue
 		}
 	}
-	return true
+}
+
+// 回退一个节点
+func (cu *ClusterUpgrade)minusAppCount(nodeName string, apps []string, SelectNodeAppsCountMap map[string]int) {
+	for i := 0; i < len(apps); i++ {
+		SelectNodeAppsCountMap[apps[i]]--
+	}
 }
 
 // 运行一次，获取剩余节点中满足可用性可重启的最长节点串
@@ -139,46 +174,63 @@ func (cu *ClusterUpgrade)GetMaxNodesToRestart() (error, map[string]struct{}) {
 	if len(cu.Node2Apps) == 0 { // 说明没有节点了
 		return nil, nil
 	}
-	// 先塞入当前节点
+	if len(cu.Nodes) == 1 { // 只有一个节点
+		return nil, map[string]struct{}{cu.Nodes[0].NodeName: {}}
+	}
+	// 先向dp table中塞入当前节点，每次只重启一个是肯定可以的
 	for i := 0; i < len(cu.Nodes); i++ {
 		map1 := make(map[string]int)
-		cu.CheckNodeAvailability(cu.Nodes[i].NodeName, cu.Node2Apps[cu.Nodes[i].NodeName], map1)
-		cu.DynamicMap[i] = Result{NodeCount: 1, SelectNodeMap: map[string]struct{}{cu.Nodes[i].NodeName: struct {}{}}, SelectNodeAppsCountMap: map1}
+		cu.addAppCount(cu.Nodes[i].NodeName, cu.Node2Apps[cu.Nodes[i].NodeName], map1)
+		results := []Result{{NodeCount: 1, SelectNodeMap: map[string]struct{}{cu.Nodes[i].NodeName: struct{}{}}, SelectNodeAppsCountMap: map1}}
+		cu.DynamicMap[i] = results
 	}
+	maxRestartNodeCount := 1
+	var maxResult Result = cu.DynamicMap[0][0] // 给个默认值
 	for i := 0; i < len(cu.Nodes); i++ { // 遍历所有节点
 		for j := 0; j < i; j++ {
-			//if  _, ok := cu.DynamicMap[j].SelectNodeMap[cu.Nodes[j].NodeName]; ok {
-			//	continue
-			//}
-			SelectNodeAppsCountMap := cu.DynamicMap[j].SelectNodeAppsCountMap // 第j个节点为结尾最长可用节点串的计数
-			// 不存在
-			if cu.CheckNodeAvailability(cu.Nodes[i].NodeName, cu.Node2Apps[cu.Nodes[i].NodeName], SelectNodeAppsCountMap) {
-				nodesMap := cu.DynamicMap[j].SelectNodeMap // 以第j个节点结尾已经选择的可用节点串
-				nodesMap[cu.Nodes[i].NodeName] = struct{}{} // 添加新节点
-				if len(nodesMap) >= len(cu.DynamicMap[i].SelectNodeMap) {
-					cu.DynamicMap[i] = Result{
-						NodeCount: len(nodesMap),
-						SelectNodeMap: nodesMap,
-						SelectNodeAppsCountMap: SelectNodeAppsCountMap,
+			results := cu.DynamicMap[j]
+			for index_result := 0; index_result < len(results); index_result++ {
+				tempMap := CopySelectNodeAppsCountMap(results[index_result].SelectNodeAppsCountMap)
+				// 剪枝
+				if cu.CheckNodeAvailability(cu.Nodes[i].NodeName, cu.Node2Apps[cu.Nodes[i].NodeName], tempMap) {
+					selectNodeAppsCountMapTemp := CopySelectNodeAppsCountMap(results[index_result].SelectNodeAppsCountMap)
+					newNodeCount := results[index_result].NodeCount + 1
+					newNodeMap := CopySelectNodeMap(results[index_result].SelectNodeMap)
+					newNodeMap[cu.Nodes[i].NodeName] = struct{}{}
+					cu.addAppCount(cu.Nodes[i].NodeName, cu.Node2Apps[cu.Nodes[i].NodeName], selectNodeAppsCountMapTemp)
+					resultTemp := Result{NodeCount: newNodeCount, SelectNodeMap: newNodeMap, SelectNodeAppsCountMap: selectNodeAppsCountMapTemp}
+					// 保存以node[i]为结尾的可行方案
+					if maxRestartNodeCount < newNodeCount {
+						maxResult = resultTemp
+						maxRestartNodeCount = newNodeCount
 					}
+					cu.DynamicMap[i] = append(cu.DynamicMap[i], resultTemp)
 				}
 			}
+
 		}
 	}
-	var result map[string]struct{}
-	maxCount := 0
-	for i := 0; i < len(cu.DynamicMap); i++ {
-		if cu.DynamicMap[i].NodeCount > maxCount {
-			maxCount = cu.DynamicMap[i].NodeCount
-			result = cu.DynamicMap[i].SelectNodeMap
-		}
-	}
-	return nil, result
+	return nil, maxResult.SelectNodeMap
 }
 
 type Result struct{
 	NodeCount int
-	SelectNodeMap map[string]struct{}
-	SelectNodeAppsCountMap map[string]int // 已选择的节点， app的数量记录 appName->count
+	SelectNodeMap map[string]struct{} // 已经选择的节点
+	SelectNodeAppsCountMap map[string]int // 已选择的节点， app的数量计数 appName->count， 便于做判断。
+}
 
+func CopySelectNodeMap(SelectNodeMap map[string]struct{} ) map[string]struct{}{
+	ret := make(map[string]struct{})
+	for k, v := range SelectNodeMap {
+		ret[k] = v
+	}
+	return ret
+}
+
+func CopySelectNodeAppsCountMap(SelectNodeAppsCountMap map[string]int) map[string]int{
+	ret := make(map[string]int)
+	for k, v := range SelectNodeAppsCountMap {
+		ret[k] = v
+	}
+	return ret
 }
